d2004fa700f4477ca3cd8102a363d9659bee2199
Mateusz <przekwasmateusz@gmail.com>
2021-10-22 10:46:20 +0200

    Opportunities and businesses added

---
+++ Admin/opportunities.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+let url = 'https://uat-forsah-api.910ths.sa/api/v1/opportunities?scope=opportunity-dashboard'
+let token = '9dd637f3cb7676f8d7c1eb496faa13fa'
+let params = {
+    headers: {
+        'X-AUTH-TOKEN': token
+    }
+}
+export default function () {
+    group('Grupa Admin Opportunities', function () {
+
+
+        let response = http.get(url, params);
+        check(response, {
+            'is status code 200': (r) => r.status === 200,
+            'is response faster then 1s': (r) => r.timings.duration < 1000,
+            'nie zwraca errora': (r) => !r.error,
+        });
+
+    });
+};
