aeb22f4fb35b32de2a820fbc56f846fd0d97e855
Mateusz <przekwasmateusz@gmail.com>
2021-10-28 15:26:24 +0200

    Test1, test file added, resources added

--- Admin/categories.js
+++ Admin/categories.js
@@ -2,13 +2,15 @@ import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
 let url = 'https://uat-forsah-api.910ths.sa/api/v1/categories?scope=admin';
-let token = '9dd637f3cb7676f8d7c1eb496faa13fa';
+let token = '9b635aac97f43938e787115b0f54db4c';
 let params = {
     headers: {
         "X-AUTH-TOKEN": token,
     }
 }
-
+export let options = {
+    Iterations: 100
+};
 export default function () {
 
     group('Admin - Catrgories', function () {
--- Admin/partners.js
+++ Admin/partners.js
@@ -4,9 +4,9 @@ import { check, group, sleep } from 'k6';
 const url = 'https://uat-forsah-api.910ths.sa/api/v1/partners?scope=ops-partner-list';
 const urlstaging = 'https://staging-forsah-api.910ths.sa/api/v1/partners?scope=ops-partner-list';
 const urlProd = 'https://forsa-api.910ths.sa/api/v1/partners?scope=ops-partner-list';
-const token = 'af771b239f5ed911e8dc2c1cc494c53e';
-const tokenstaging = 'e7a84f1523f0fd57806630c0e2ae7021';
-const tokenProd = 'fa896529ac490ea4dd1dac9e7bae207a';
+const token = '9b635aac97f43938e787115b0f54db4c';
+const tokenstaging = '9b635aac97f43938e787115b0f54db4c';
+const tokenProd = '9b635aac97f43938e787115b0f54db4c';
 const params = {
     headers: {
         "X-AUTH-TOKEN": token,
@@ -22,12 +22,14 @@ const paramsProd = {
         "X-AUTH-TOKEN": tokenProd,
     }
 }
-
+export let options = {
+    Iterations: 100
+};
 export default function () {
 
     let res = http.get(url, params);
-    let res2 = http.get(urlstaging, paramsStaging);
-    let res3 = http.get(urlProd, paramsProd);
+    //let res2 = http.get(urlstaging, paramsStaging);
+    //let res3 = http.get(urlProd, paramsProd);
 
     check(res, {
         'is status code 200': (r) => r.status === 200,
