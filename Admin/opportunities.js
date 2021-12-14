d28c7e8a365d2037fbac65501b7d9e828b119b2e
Mateusz <przekwasmateusz@gmail.com>
2021-12-14 12:39:52 +0100

    Improvementy po kursie

--- Admin/opportunities.js
+++ Admin/opportunities.js
@@ -5,9 +5,18 @@ import { rUrl, params } from './resources.js'
 let url = `${rUrl}/api/v1/opportunities?scope=opportunity-dashboard`;
 
 export let options = {
-    Iterations: 100
+    Iterations: 1,
+    //Iterations: __ENV.batch || 100 - zlini polecen
+    thresholds: { //quality gate'y
+        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+        checks: ['rate>0.6'], //checki mogą być również użyte w thresholdach
+        'group_duration{group:::Grupa Admin Opportunities}': ['avg < 10'],
+    },
 };
-
+export function setup() {
+    let response = http.get(url, params);
+    return response
+  }
 export default function () {
     group('Grupa Admin Opportunities', function () {
 
@@ -16,7 +25,15 @@ export default function () {
         check(response, {
             'is status code 200': (r) => r.status === 200,
             'is response faster then 1s': (r) => r.timings.duration < 1000,
-            'nie zwraca errora': (r) => !r.error,
+            'body odpowiedzi nie zwraca errora': (r) => !r.error,
+
         });
+       // console.log(JSON.stringify(response,null,2));
+       
     });
 };
+export function teardown(data) {
+    console.log("KONIEC")
+    console.log(JSON.stringify(data,null,2));
+    //  https://github.com/gabrielstar/k6training/blob/master/007_structure/01_teoria.md
+  }
--- Admin/resources.js
+++ Admin/resources.js
@@ -1,5 +1,5 @@
-//let rUrl = 'https://uat-forsah-api.910ths.sa'
-let rUrl = 'https://forsa-api.910ths.sa'
+let rUrl = 'https://uat-forsah-api.910ths.sa'
+//let rUrl = 'https://forsa-api.910ths.sa'
 
 let rToken = '4cf3f029c31a1bf24244eaf3c1b29d95'
 let params = {
@@ -10,3 +10,11 @@ let params = {
 module.exports = {
     rToken, rUrl, params
  }
+// module.export zamiast tego przed let moge dac export 
+
+//jak nie chce mi sie importowac pojedynczo to tez wyeksportowac moge jako 1 obiekt 
+//wszystko z góry i zponiej zaimportowac tlyko ten 1 obiekt
+//export const obiektAll = {
+//params: params,
+
+//}
