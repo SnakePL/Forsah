5a9e73da4112b52703e72fe64816451ad33379ef
Mateusz <przekwasmateusz@gmail.com>
2022-03-02 08:26:14 +0100

    Aktualizacja Wynikow w numbers, dodanie cr-UAT do resources

--- Admin/resources.js
+++ Admin/resources.js
@@ -1,7 +1,8 @@
 //let rUrl = 'https://uat-forsah-api.910ths.sa'
-let rUrl = 'https://forsa-api.910ths.sa'
+//let rUrl = 'https://forsa-api.910ths.sa'
+let rUrl = 'https://cr-forsah-uat-api.k8s.910ths.sa'
 
-let rToken = '4cf3f029c31a1bf24244eaf3c1b29d95'
+let rToken = 'cdd0df0fe5e1d0269ae71f2f5ff72dca'
 let params = {
     headers: {
         'X-AUTH-TOKEN': rToken
