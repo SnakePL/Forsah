b66cad5f58956763da0e1b404a77abc6dfef2ee5
SnakePL <przekwasmateusz@gmail.com>
2022-04-01 09:35:45 +0200

    2.1.0 prod

--- Admin/resources.js
+++ Admin/resources.js
@@ -1,8 +1,8 @@
-let rUrl = 'https://uat-forsah-api.910ths.sa'
-//let rUrl = 'https://forsa-api.910ths.sa'
+//let rUrl = 'https://uat-forsah-api.910ths.sa'
+let rUrl = 'https://forsa-api.910ths.sa'
 //let rUrl = 'https://cr-forsah-uat-api.k8s.910ths.sa'
 
-let rToken = 'f80823a314ebcfac27435708947bda08'
+let rToken = 'b5c6e4b37568e8a65de32b7134ca1f7e'
 let params = {
     headers: {
         'X-AUTH-TOKEN': rToken
