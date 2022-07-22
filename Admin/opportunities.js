5076ca383feca73a934f1479b56da74b4519d381
SnakePL <przekwasmateusz@gmail.com>
2022-07-22 08:59:55 +0200

    check improvement

--- Admin/opportunities.js
+++ Admin/opportunities.js
@@ -14,9 +14,10 @@ export default function () {
 
         let response = http.get(url, params);
         check(response, {
-            'is status code 200': (r) => r.status === 200,
-            'is response faster then 1s': (r) => r.timings.duration < 1000,
-            'nie zwraca errora': (r) => !r.error,
+            'Is status code 200': (r) => r.status === 200,
+            'Is response faster than 1s': (r) => r.timings.duration < 1000,
+            'Is response faster than 2s': (r) => r.timings.duration < 2000,
+            'Is there an error in the response': (r) => !r.error,
         });
     });
 };
--- Admin/resources.js
+++ Admin/resources.js
@@ -1,8 +1,8 @@
-//let rUrl = 'https://uat-forsah-api.910ths.sa'
-let rUrl = 'https://forsa-api.910ths.sa'
+let rUrl = 'https://uat-forsah-api.910ths.sa'
+//let rUrl = 'https://forsa-api.910ths.sa'
 //let rUrl = 'https://cr-forsah-uat-api.k8s.910ths.sa'
 
-let rToken = '3ab3ce258e6cedb41bf6eb920cf113e1'
+let rToken = 'd44f2afacc48d74f33fa9093627884a3'
 let params = {
     headers: {
         'X-AUTH-TOKEN': rToken
